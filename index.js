4c06ba3a03ea0043fbe0bd90cf8a719f2baebe0b
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-11 21:51:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+
+//Create a new element here and assin it to a newHeader
+const newHeader = document.createElement("h1");
+newHeader.id = ('victory');
+newHeader.victory = document.body.appendChild(newHeader);
+newHeader.innerHTML = "<h1>penina is the champion!</h1>"; 
